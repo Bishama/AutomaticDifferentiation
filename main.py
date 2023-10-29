343087524624072cde80dc4d5bee99f64bc783f7
Bishama Irfan <bishamairfann10@gmail.com>
2023-10-29 11:52:26 +0100

    Added children and op to keep track

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ import matplotlib.pyplot as plt
 
 class Value:
     
-    def __init__(self, data):
+    def __init__(self, data), _children=(), _op='':
         self.data=data
+        self.prev=set(_children)   
+        self.op = _op
         
     def __repr__(self):
         return f"Value(data={self.data})"
     
     def __add__(self,other):
-        out = Value(self.data + other.data)
+        out = Value(self.data + other.data, (self,other), '+')
         return out
     def __mul__(self,other):
-        out = Value(self.data * other.data)
+        out = Value(self.data * other.data, (self,other), '*')
         return out
